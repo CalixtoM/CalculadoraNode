871cc150cb3fb1ac401b5cbe7f5d564062d35754
Matheus Calixto <mcalixto2003@gmail.com>
2021-02-12 22:34:51 -0300

    commit inicial

---
+++ calc.js
@@ -0,0 +1,9 @@
+var somafunc = require("./somar");
+var subfunc = require("./subtrair");
+var multifunc = require("./multiplicar");
+var divfunc = require("./dividir");
+
+console.log(somafunc(4,5))
+console.log(subfunc(10,2))
+console.log(multifunc(5,4))
+console.log(divfunc(4,2))
